5271a4a8da41c3e019d7b1ab809e0c8c469e02c9
Tru Trong Bui Jr <bui46@data.cs.purdue.edu>
2025-01-22 10:39:54 -0500

    Commit

--- myMalloc.c
+++ myMalloc.c
@@ -191,7 +191,14 @@ static inline header * allocate_object(size_t raw_size) {
   if (raw_size == 0) {
         return NULL;
   }
-  size_t actual_size = ALIGN(raw_size + sizeof(header));
+
+  /* calculate actual size and corresponding index */
+
+  size_t aligned_raw_size = ALIGN(raw_size);
+  size_t actual_size = aligned_raw_size = 16;
+  if (actual_size < sizeof(header)) {
+    acutal_size = sizeof(header);
+  }
   int free_list_index = (actual_size / 8) - 1;
 
   /* iterate through free_lists until reach non empty OR last list*/
