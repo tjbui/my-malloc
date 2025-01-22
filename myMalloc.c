b813b5b3bab6fda191938c1ee855975483da6a6a
Tru Trong Bui Jr <bui46@data.cs.purdue.edu>
2025-01-21 22:43:09 -0500

    Commit

--- myMalloc.c
+++ myMalloc.c
@@ -244,6 +244,7 @@ static inline header * allocate_object(size_t raw_size) {
           remaining_block->next = new_sentinel->next;
           remaining_block->prev = new_sentinel;
           new_sentinel->next = remaining_block;
+          new_sentinel -> next = remaining_block;
         }
         current->size_state = actual_size;
         current->size_state |= ALLOCATED;
