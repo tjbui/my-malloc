918ee455a86f1ef4d52c2f8b3c020b812bfe0e8d
Tru Trong Bui Jr <bui46@data.cs.purdue.edu>
2025-01-27 15:20:02 -0500

    Commit

--- myMalloc.c
+++ myMalloc.c
@@ -464,32 +464,10 @@ static inline void deallocate_object(void * p) {
       /* stay in same spot of free list if last list */
 
       if (get_free_list_index(get_size(left)) == 58) {
-
-        // Traverse the last free list to find the right block
-
-/*
-        header *current_in_list = freelistSentinels[58].next;
-        while (current_in_list != &freelistSentinels[58]) {
-            if (current_in_list == left) {
-
-                // Found the right block; update it directly
-
-                set_size(current_in_list, new_size);
-                set_state(current_in_list, UNALLOCATED);
-                header *new_right = get_right_header(current_in_list);
-                new_right -> left_size = new_size;
-                break;
-            }
-            current_in_list = current_in_list->next;
-        }
-*/  
         set_size(left, new_size);
         set_state(left, UNALLOCATED);
         header *new_right = get_right_header(current_in_list);
         new_right -> left_size = new_size;
-
-        
-
       }
       else {
         remove_header(left);
@@ -504,16 +482,24 @@ static inline void deallocate_object(void * p) {
 
       // Case 4: Both neighbors are unallocated
 
-      set_state(current, UNALLOCATED);
-      size_t new_size = current_size + get_size(right) + get_size(left);
-      remove_header(right);
-      remove_header(left);
-      set_state(left, UNALLOCATED);
-      set_size(left, new_size);
-      header *new_right = get_right_header(left);
-      new_right->left_size = new_size;
-      int free_list_index = get_free_list_index(new_size);
-      insert_header(left, free_list_index);
+      if (get_free_list_index(get_size(left)) == 58) {
+        printf("test");
+      }
+      else if (get_free_list_index(get_size(right)) == 58) {
+        printf("test");
+      }
+      else {
+        set_state(current, UNALLOCATED);
+        size_t new_size = current_size + get_size(right) + get_size(left);
+        remove_header(right);
+        remove_header(left);
+        set_state(left, UNALLOCATED);
+        set_size(left, new_size);
+        header *new_right = get_right_header(left);
+        new_right->left_size = new_size;
+        int free_list_index = get_free_list_index(new_size);
+        insert_header(left, free_list_index);
+      }
   }
 }
 
