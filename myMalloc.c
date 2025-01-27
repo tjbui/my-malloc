baaecad4f23122f8d35b8e40bf955ceeeb144842
Tru Trong Bui Jr <bui46@data.cs.purdue.edu>
2025-01-27 15:11:31 -0500

    Commit

--- myMalloc.c
+++ myMalloc.c
@@ -439,39 +439,11 @@ static inline void deallocate_object(void * p) {
       /* stay in same spot of free list if last list*/
 
       if (get_free_list_index(get_size(right)) == 58) {
-
-        // Traverse the last free list to find the right block
-
-
-
-/*
-        header *current_in_list = freelistSentinels[58].next;
-        while (current_in_list != &freelistSentinels[58]) {
-            if (current_in_list == right) {
-
-                // Found the right block; update it directly
-
-                set_size(current_in_list, new_size);
-                current_in_list->left_size = current -> left_size;
-                set_state(current_in_list, UNALLOCATED);
-                header *new_right = get_right_header(current_in_list);
-                new_right -> left_size = new_size;
-                break;
-            }
-            current_in_list = current_in_list->next;
-        }
-*/
-
-
-
-
         set_size(right, new_size);
         right -> left_size = current -> left_size;
         set_state(right, UNALLOCATED);
         header *new_right = get_right_header(right);
         new_right -> left_size = new_size;
- 
- 
       }
       else {
         remove_header(right);
@@ -488,10 +460,14 @@ static inline void deallocate_object(void * p) {
 
       set_state(current, UNALLOCATED);
       size_t new_size = current_size + get_size(left);
+
+      /* stay in same spot of free list if last list */
+
       if (get_free_list_index(get_size(left)) == 58) {
 
         // Traverse the last free list to find the right block
 
+/*
         header *current_in_list = freelistSentinels[58].next;
         while (current_in_list != &freelistSentinels[58]) {
             if (current_in_list == left) {
@@ -505,7 +481,15 @@ static inline void deallocate_object(void * p) {
                 break;
             }
             current_in_list = current_in_list->next;
-        } 
+        }
+*/  
+        set_size(left, new_size);
+        set_state(left, UNALLOCATED);
+        header *new_right = get_right_header(current_in_list);
+        new_right -> left_size = new_size;
+
+        
+
       }
       else {
         remove_header(left);
