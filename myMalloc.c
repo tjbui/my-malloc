3affadb870da781f7e2b305271dd80ef3a26d7c0
Tru Trong Bui Jr <bui46@data.cs.purdue.edu>
2025-01-30 14:24:19 -0500

    Commit

--- myMalloc.c
+++ myMalloc.c
@@ -678,17 +678,17 @@ void * my_realloc(void * ptr, size_t size) {
   header * right = get_right_header(header);
   size_t new_size = calculate_actual_size(size);
   
-  if ((get_state(right) == UNALLOCATED) && ((get_size(header) + get_size(right)) >= new_size)) {
-    set_size(header, new_size);
-  }
-  else {
+  //if ((get_state(right) == UNALLOCATED) && ((get_size(header) + get_size(right)) >= new_size)) {
+  //  set_size(header, new_size);
+ // }
+  //else {
 
     void * mem = my_malloc(size);
     memcpy(mem, ptr, size);
     my_free(ptr);
     return mem;
 
-  }
+  //}
 }
 
 void my_free(void * p) {
