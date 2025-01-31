debecdd309cbc37aebdea7065e49620a79a1aab3
Tru Trong Bui Jr <bui46@data.cs.purdue.edu>
2025-01-31 16:48:10 -0500

    Commit

--- myMalloc.c
+++ myMalloc.c
@@ -277,6 +277,9 @@ static header * split_if_necessary(header * current, size_t actual_size, int fre
           set_size(allocated_block, actual_size);
           allocated_block -> left_size = get_size(remaining_block);
           set_state(allocated_block, ALLOCATED);
+          
+          check_free_list_index(get_free_list_index(get_size(remaining_block)));
+
           header *right_block = get_right_header(allocated_block);
           if (get_size(right_block) != 0) {
             right_block->left_size = get_size(allocated_block);
